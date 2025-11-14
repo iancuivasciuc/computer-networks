02fc3243343ddca6fbafb81b5043691c1991c313
NFJJunior <iancu_ivasciuc@yahoo.com>
2025-11-14 18:53:57 +0100

    fix: remove errors from get_parser_html

--- capitolul2/src/get_parse_html.py
+++ capitolul2/src/get_parse_html.py
@@ -10,14 +10,13 @@ headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36"
 }
 
-response = requests.get('http://fmi.unibuc.ro/ro', headers=headers)
+response = requests.get('https://fmi.unibuc.ro', headers=headers)
 print (response.text[:200])
 
 # proceseaza continutul html
-supa = BeautifulSoup(response.text)
+supa = BeautifulSoup(response.text, features="html.parser")
 
-# cauta div cu class cst2
-div = supa.find('div', {'class': 'cst2'})
-paragraph = div.find('p')
+# cauta lista cu clasa ultimeleor postari
+div = supa.find('ul', {'class': 'wp-block-latest-posts__list'})
 
-print (paragraph.text)
+print(div.text)
